6948e37fef07a477a575d202750fd26deeb17167
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 03:53:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const discord = require("discord.js")
 const bot = new discord.Client()
+const config = require('./config.json')
 
 bot.on('ready', () => {
     bot.user.setActivity("jablonska kurewica")
@@ -11,5 +12,63 @@ bot.on('message', message => {
         message.channel.send("pong!")
     }
 })
+
+const completemsg = `Zostałeś pomyślnie zweryfikowany. Dziękujemy! \nTeraz możesz udać się na serwer i wybrać rangi które cię interesują. Nie zapomnij napisać Cześć do wszystkich!`
+
+const shortcode = (n) => {
+    const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
+    let text = ''
+    for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
+    return text;
+}
+
+client.on('guildMemberAdd', (member) => {
+    if (member.user.bot || member.guild.id !== config.guild) return
+    const token = shortcode(8)
+    const welcomemsg = `**Hej! Zweryfikuj konto** Pamiętaj, aby przed weryfikacją zerknąć na kanał \`#zasady\` i \`#informacje\` \n\n Weryfikując swoje konto oświadczasz, że masz minimum 13 lat, proszę przepisz poniższy kod. \n\n\`${token}\` \n\n\Nie możesz się zweryfikować? Skontaktuj się z administratorem serwera..`
+    console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
+    member.send(welcomemsg)
+    member.user.token = token
+})
+
+const verifymsg = '{token}'
+
+client.on('message', (message) => {
+    if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
+    if (message.content !== (verifymsg.replace('{token}', message.author.token))) return
+    message.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: completemsg,
+            timestamp: new Date(),
+            footer: {
+                text: `Przepisując kod automatycznie zaakceptowałeś regulamin serwera.`
+            }
+        }
+    })
+    client.guilds.get("746030108460056718").member(message.author).addRole("922282005385863190");
+})
+
+client.on('disconnect', (event) => {
+    setTimeout(() => client.destroy().then(() => client.login(config.token)), 10000)
+    console.log(`[DISCONNECT] Notice: Disconnected from gateway with code ${event.code} - Attempting reconnect.`)
+})
+
+client.on('reconnecting', () => {
+    console.log(`[NOTICE] ReconnectAction: Reconnecting to Discord...`)
+})
+
+client.on('error', console.error)
+client.on('warn', console.warn)
+
+process.on('unhandledRejection', (error) => {
+    console.error(`Uncaught Promise Error: \n${error.stack}`)
+})
+
+process.on('uncaughtException', (err) => {
+    let errmsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
+    console.error(errmsg)
+})
+
 
 bot.login(process.env.BOT_TOKEN);
