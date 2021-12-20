28425c92a96496f0ff45d081b847226dea7b8bda
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 11:35:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,4 +82,36 @@ client.on('ready', () => {
     console.log("Logged in successfully :D")
 })
 
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'witamy');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./welcome-image.png');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.font = '35px impact';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Witaj na serwerze', canvas.width / 3.2, canvas.height / 3.5);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 3.2, canvas.height / 1.7);
+
+	ctx.beginPath();
+	ctx.arc(120, 125, 90, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+	const avatar = await Canvas.loadImage(buffer);
+	ctx.drawImage(avatar, 20, 25, 190, 190);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`:eggplant: **${member}** **Surprise bitch! I bet you thought you'd seen the last of me**`, attachment);
+});
+
 client.login(process.env.BOT_TOKEN);
