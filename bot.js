94ac6ad2d4046e0f0c771e98bb20d41ac8c28c45
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 22:08:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -221,7 +221,7 @@ client.on('guildMemberAdd', async member => {
         .setLevelColor(color, "COLOR")
         .setUsername(rankuser.username, color)
         .setRank(Number(i), "Twoja ranga", true)
-        .setLevel(Number(client.points.get(key, `level`)), "Poziom", true)
+        .setLevel(Number(client.points.get(key, `level`)), "Level", true)
         .setDiscriminator(rankuser.discriminator, color);
       rank.build()
         .then(async data => {
