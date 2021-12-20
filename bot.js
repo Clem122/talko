898c981dd57c3f575befa213bc3c2746265f7fa1
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 04:26:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json')
 const prefix = "$";
 
 client.on('ready', () => {
@@ -11,6 +12,12 @@ client.on('message', message => {
     if(message.content === "!ping") {
         message.channel.send("pong!")
     }
+})
+
+client.on('guildMemberAdd', (member) => {
+    if (member.user.bot || member.guild.id !== config.guild) return
+    const welcomemsg = `**Hej! Zweryfikuj konto** Pamiętaj, aby przed weryfikacją zerknąć na kanał \`#zasady\` i \`#informacje\` \n\n Weryfikując swoje konto oświadczasz, że masz minimum 13 lat, proszę przepisz poniższy kod. \n\n\`${token}\` \n\n\Nie możesz się zweryfikować? Skontaktuj się z administratorem serwera..`
+    member.send(welcomemsg)
 })
 
 client.on("ready", () => {
