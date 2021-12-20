c7a9938d75fbb642078fc3f106b0375e233c4f8a
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 10:46:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,9 +39,8 @@ client.on('message', (message) => {
             }
         }
     })
-    const aihefhiwebf = client.guilds.cache.get("922242038722887751")
-    aihefhiwebf.member(message.author).addRole("922282005385863190");
-    aihefhiwebf.member(message.author).removeRole("922283217170599957");
+    const verify = message.guild.roles.cache.get('922282005385863190');
+    member.roles.add(verify).catch(console.error);
 })
 
 client.on('disconnect', (event) => {
