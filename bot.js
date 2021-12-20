44990329c8ac7c9946d1e9ef5b0a50564082b084
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 12:07:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const Discord = require("discord.js");
+const client = new Discord.Client();
 const config = require('./config.json')
 const ms = require("ms");
 const fs = require("fs");
