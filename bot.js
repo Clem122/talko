db2cd9bb814f48074571ccf1377ce18e960a4e39
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 03:32:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,8 +46,8 @@ client.on('message', (message) => {
             }
         }
     })
-    client.guilds.get("746030108460056718").member(message.author).addRole("746037675768938546");
-    client.guilds.get("746030108460056718").member(message.author).removeRole("746076005650792518");
+    client.guilds.get("746030108460056718").member(message.author).addRole("922282005385863190");
+    client.guilds.get("746030108460056718").member(message.author).removeRole("922283217170599957");
 })
 
 client.on("message", async message => {
