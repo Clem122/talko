542b80ae7db74ec56811c688385b52569d0111f4
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 04:09:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const discord = require("discord.js")
-const client = new discord.Client()
+const config = require('./config.json')
+const client = new discord.Client();
 
 client.on('ready', () => {
     bot.user.setActivity("jablonska kurewica")
@@ -12,22 +13,10 @@ client.on('message', message => {
     }
 })
 
-const completemsg = `Zostałeś pomyślnie zweryfikowany. Dziękujemy! \nTeraz możesz udać się na serwer i wybrać rangi które cię interesują. Nie zapomnij napisać Cześć do wszystkich!`
-
-const shortcode = (n) => {
-    const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
-    let text = ''
-    for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
-    return text;
-}
-
 client.on('guildMemberAdd', (member) => {
     if (member.user.bot || member.guild.id !== config.guild) return
-    const token = shortcode(8)
     const welcomemsg = `**Hej! Zweryfikuj konto** Pamiętaj, aby przed weryfikacją zerknąć na kanał \`#zasady\` i \`#informacje\` \n\n Weryfikując swoje konto oświadczasz, że masz minimum 13 lat, proszę przepisz poniższy kod. \n\n\`${token}\` \n\n\Nie możesz się zweryfikować? Skontaktuj się z administratorem serwera..`
-    console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
     member.send(welcomemsg)
-    member.user.token = token
 })
 
 client.on("ready", () => {
