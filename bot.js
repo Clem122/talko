909f722eb6d7e3a12acb302bb89e8706bc8253b5
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 03:28:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,68 @@ client.on('message', message => {
     }
 })
 
+const completemsg = `Zostałeś pomyślnie zweryfikowany. Dziękujemy! \nTeraz możesz udać się na serwer i wybrać rangi które cię interesują. Nie zapomnij napisać Cześć do wszystkich!`
+
+const shortcode = (n) => {
+    const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
+    let text = ''
+    for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
+    return text;
+}
+
+client.on('guildMemberAdd', (member) => {
+    if (member.user.bot || member.guild.id !== config.guild) return
+    const token = shortcode(8)
+    const welcomemsg = `**Hej! Zweryfikuj konto** Pamiętaj, aby przed weryfikacją zerknąć na kanał \`#zasady\` i \`#informacje\` \n\n Weryfikując swoje konto oświadczasz, że masz minimum 13 lat, proszę przepisz poniższy kod. \n\n\`${token}\` \n\n\Nie możesz się zweryfikować? Skontaktuj się z administratorem serwera..`
+    console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
+    member.send(welcomemsg)
+    member.user.token = token
+})
+
+const verifymsg = '{token}'
+
+client.on('message', (message) => {
+    if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
+    if (message.content !== (verifymsg.replace('{token}', message.author.token))) return
+    message.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: completemsg,
+            timestamp: new Date(),
+            footer: {
+                text: `Przepisując kod automatycznie zaakceptowałeś regulamin serwera.`
+            }
+        }
+    })
+    client.guilds.get("746030108460056718").member(message.author).addRole("746037675768938546");
+    client.guilds.get("746030108460056718").member(message.author).removeRole("746076005650792518");
+})
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.indexOf(prefix) !== 0) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Let's go with a few common example commands! Feel free to delete or change those.
+
+    if (command === "nigdytegoniezgadniesz913xdjg") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o => { });
+        // And we get the bot to say the thing:
+        const embed = {
+            "title": "Hej! Zweryfikuj konto",
+            "description": `${sayMessage}`,
+            "color": 11041206
+        };
+        message.channel.send({ embed });
+}
+});
+
 client.login(process.env.BOT_TOKEN);
