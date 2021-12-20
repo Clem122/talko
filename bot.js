dd7d4a764724b4bb98379f1296c623d57b725446
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 22:11:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,11 +94,11 @@ client.on('guildMemberAdd', async member => {
 
 	ctx.font = '37px Genta';
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Witaj na serwerze', canvas.width / 2.5, canvas.height / 3.5);
+	ctx.fillText('Witaj na serwerze', canvas.width / 1.5, canvas.height / 3.5);
 
 	ctx.font = applyText(canvas, `${member.displayName}!`);
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.7);
+	ctx.fillText(`${member.displayName}!`, canvas.width / 1.5, canvas.height / 1.7);
 
 	ctx.beginPath();
 	ctx.arc(120, 125, 90, 0, Math.PI * 2, true);
