df7e0b78f7027d2f23cc81be28d8ff92d1ea106e
Clem122 <69987127+Clem122@users.noreply.github.com>
2021-12-20 11:51:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,20 +120,4 @@ client.on('ready', () => {
     console.log("Logged in successfully :D")
 })
 
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
-
-	// Declare a base size of the font
-	let fontSize = 70;
-
-	do {
-		// Assign the font to the context and decrement it so it can be measured again
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-		// Compare pixel width of the text to the canvas minus the approximate avatar size
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	// Return the result to use in the actual canvas
-	return ctx.font;
-};
-
 client.login(process.env.BOT_TOKEN);
